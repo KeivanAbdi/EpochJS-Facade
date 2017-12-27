d13ab4b4d3bc9301815977c7a7ef6fd08e14d5c8
Keivan A <keivan.a.khorsand@gmail.com>
2017-12-27 22:37:01 +0330

    :bug:Fix wrong type

--- src/main/scala/com/keivanabdi/scalajs/epochjs/chart/basic/line/Value.scala
+++ src/main/scala/com/keivanabdi/scalajs/epochjs/chart/basic/line/Value.scala
@@ -1,13 +1,13 @@
 package com.keivanabdi.scalajs.epochjs.chart.basic.line
 
 class Value(
-    val x: Int,
+    val x: Double,
     val y: Double
 ) extends scalajs.js.Object
 
 object Value {
   def apply(
-      x: Int,
+      x: Double,
       y: Double
   ): Value = new Value(x, y)
 }
