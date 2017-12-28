15591c58a17f8354f985177f8003e82c5972bbf4
Keivan A <keivan.a.khorsand@gmail.com>
2017-12-28 23:51:48 +0330

    :bug:Fix typo bug

--- src/main/scala/com/keivanabdi/scalajs/epochjs/chart/realtime/bar/Value.scala
+++ src/main/scala/com/keivanabdi/scalajs/epochjs/chart/realtime/bar/Value.scala
@@ -1,13 +1,13 @@
 package com.keivanabdi.scalajs.epochjs.chart.realtime.bar
 
 class Value(
-    val x: Long,
-    val y: Double,
+    val time: Long,
+    val y:    Double
 ) extends scalajs.js.Object
 
 object Value {
   def apply(
-      x: Long,
-      y: Double,
-  ): Value = new Value(x, y)
+      time: Long,
+      y:    Double
+  ): Value = new Value(time, y)
 }
